4a1562043616d2677538556fcaf5898d3cea78f0
dombeesley <quigley_beesley@hotmail.com>
2020-04-29 07:16:00 +0000

    add todo_item

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=9@%ftb74w@bz4zd%kr^zn!jt7offju&_(()&ac96!#hbg_8wm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
