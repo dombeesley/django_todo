aed366a5dffdd0adba62f49a308e30279f4bb730
dombeesley <quigley_beesley@hotmail.com>
2020-05-05 07:53:47 +0000

    edit settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -18,6 +18,7 @@ if os.environ.get('DEVELOPMENT'):
 else:
     development = False
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
